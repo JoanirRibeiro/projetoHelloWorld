95af71823359bc01f606ba7aad5555511b04a9a2
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-05-14 22:46:07 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let nomeUsuário = prompt("Qual o seu nome?");
+let nomeUsuário = "" ;
 let elemento = document.querySelector("#nome-usuário");
+
+
+while(nomeUsuário == "") {
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
 elemento.textContent = nomeUsuário;
